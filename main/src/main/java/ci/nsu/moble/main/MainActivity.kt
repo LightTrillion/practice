3ec87580261e8128bc80902c63211a26f4f818b8
Vorobevev <borlor231@gmail.com>
2026-02-12 16:05:46 +0700

    Таск 2, начало 120226

--- main/src/main/java/ci/nsu/moble/main/MainActivity.kt
+++ main/src/main/java/ci/nsu/moble/main/MainActivity.kt
@@ -11,37 +11,40 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import ci.nsu.moble.main.ui.theme.Blue6
+import ci.nsu.moble.main.ui.theme.Green4
+import ci.nsu.moble.main.ui.theme.Light_Blue5
+import ci.nsu.moble.main.ui.theme.Orange2
 import ci.nsu.moble.main.ui.theme.PracticeTheme
+import ci.nsu.moble.main.ui.theme.Red1
+import ci.nsu.moble.main.ui.theme.Violet7
+import ci.nsu.moble.main.ui.theme.Yellow3
+
+private val colorsMap = mapOf(
+    "Red" to Red1,
+    "Orange" to Orange2,
+    "Yellow" to Yellow3,
+    "Green" to Green4,
+    "Light_Blue" to Light_Blue5,
+    "Blue" to Blue6,
+    "Violet" to Violet7
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PracticeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            PracticeTheme() {
+                Main()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Main(modifier: Modifier = Modifier) {
+    Scaffold {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PracticeTheme {
-        Greeting("Android")
     }
 }
