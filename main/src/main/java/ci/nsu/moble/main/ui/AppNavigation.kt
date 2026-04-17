d840a16f4f79b53a5c0dbfe421c1e9244274f9e5
RomanovRO <romanovro@mer.ci.nsu.ru>
2026-04-17 11:24:54 +0700

    fix logout

--- main/src/main/java/ci/nsu/moble/main/ui/AppNavigation.kt
+++ main/src/main/java/ci/nsu/moble/main/ui/AppNavigation.kt
@@ -28,16 +28,19 @@ fun AppNavigation() {
         composable("login") { LoginScreen(navController, authViewModel) }
         composable("register") { RegisterScreen(navController, authViewModel) }
         composable("main_flow") {
-            MainContainerScreen(rootNavController = navController, factory = factory)
+            MainContainerScreen(
+                rootNavController = navController,
+                factory = factory,
+                authViewModel = authViewModel
+            )
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainContainerScreen(rootNavController: NavController, factory: ViewModelFactory) {
+fun MainContainerScreen(rootNavController: NavController, factory: ViewModelFactory, authViewModel: AuthViewModel) {
     val bottomNavController = rememberNavController()
-    val authViewModel: AuthViewModel = viewModel(factory = factory)
 
     Scaffold(
         topBar = {
@@ -47,7 +50,7 @@ fun MainContainerScreen(rootNavController: NavController, factory: ViewModelFact
                     TextButton(onClick = {
                         authViewModel.logout()
                         rootNavController.navigate("login") {
-                            popUpTo("main_flow") { inclusive = true }
+                            popUpTo(0) { inclusive = true }
                         }
                     }) { Text("Выход", color = MaterialTheme.colorScheme.error) }
                 }
